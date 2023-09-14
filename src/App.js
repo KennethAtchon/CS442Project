49d4818eb6df6e20a128f1ae299794c8c59a7a5e
Kenneth <kenneth.atchon@hotmail.com>
2023-09-14 08:24:57 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import LoginIn from './routes/login'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Routes>
+      
+      <Route path="/" element={ < LoginIn /> } />
+      
+    </Routes>
+
   );
 }
+
+// home page -> options to sign in or log in or stay as guest
+// maybe home page shows recently purchased items
+// maybe a tabbar containing the different products and current products
+// products page -> will be same if they search or clicked on a tab bar category
+// individual product page -> were user can write reviews under
+// reviews page
+// FAQ page
 
 export default App;
